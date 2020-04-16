451cb3c58922ceea9da3663b85b4038576ebea39
Halász Viktória <halaszwiki@gmail.com>
2020-04-16 22:49:52 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Login } from './Login'
+import { proxy } from './chat.d';
+import { Main } from './Main';
+
+export default class App extends Component {
+    state = { showLogin: true };
+
+    componentDidMount() {
+        proxy.addEventListener("login", () => { this.setState({ showLogin: false }) });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                {this.state.showLogin ? <Login /> : <Main />}
+            </div>
+        );
+    }
+
+}
---
+++ src/Main.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { LeftPane } from './LeftPane';
+import { RightPane } from './RightPane';
+import { ConversationDto, proxy } from './chat.d';
+
+export class Main extends Component
+{
+    state = { selectedConversation: undefined as ( ConversationDto | undefined ) };
+
+    render()
+    {
+        let className = "main row " + ( this.state.selectedConversation ? "right" : "left" );
+        return (
+            <div className={ className }>
+                <LeftPane
+                    inbox={ proxy.inbox! }
+                    selectedConversation={ this.state.selectedConversation }
+                    onSelect={ c => this.setState( { selectedConversation: c } ) } />
+                <RightPane conversation={ this.state.selectedConversation }
+                    onBack={ () => this.setState( { selectedConversation: undefined } ) } />
+            </div>
+        );
+    }
+
+}
