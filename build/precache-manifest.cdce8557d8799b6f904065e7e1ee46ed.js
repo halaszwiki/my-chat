c1b69b96e0cfdc3ac79ff2a89bb56b24954867cd
Halász Viktória <halaszwiki@gmail.com>
2020-04-16 22:52:37 +0200

    second commit

---
+++ build/precache-manifest.cdce8557d8799b6f904065e7e1ee46ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb62cbacfd28bd0ecabff5b8415c0af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f31cc76cfdc91c070862",
+    "url": "./static/css/main.9fb27002.chunk.css"
+  },
+  {
+    "revision": "a3d83ee08f09b3ce8c6d",
+    "url": "./static/js/2.2fb3bdb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2fb3bdb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31cc76cfdc91c070862",
+    "url": "./static/js/main.0f66535a.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
